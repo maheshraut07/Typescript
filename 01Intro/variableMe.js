5a354871d1129e7ebffba245ac2e9e6d43a4af52
maheshraut07 <rautmahesh213@gmail.com>
2025-01-16 21:53:24 +0530

    declared number, boolean etc. type

---
+++ 01Intro/variableMe.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "hello mahesh";
+// greetings = 6;
+var userid = 36453;
+userid.toFixed();
+var isloggedIn = false;
+console.log(greetings);
---
+++ 01Intro/variableMe.ts
@@ -0,0 +1,11 @@
+let greetings : string = "hello mahesh";
+
+// greetings = 6;
+
+let userid  = 36453
+userid.toFixed()
+
+let isloggedIn : boolean = false;
+console.log(greetings);
+
+export {}
