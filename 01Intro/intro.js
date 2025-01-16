e0352b1cfc1b654692bc00dbdb2b97fea86d00c8
maheshraut07 <rautmahesh213@gmail.com>
2025-01-16 14:11:20 +0530

    intro is completed

---
+++ 01Intro/intro.js
@@ -0,0 +1,4 @@
+var user = { name: "mahesh", age: 22 };
+console.log("mahesh");
+console.log(user.name);
+console.log(user.email);
---
+++ 01Intro/intro.ts
@@ -0,0 +1,5 @@
+let user = {name : "mahesh", age : 22}
+
+console.log("mahesh")
+console.log(user.name)
+console.log(user.email)
